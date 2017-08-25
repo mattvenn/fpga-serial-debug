26addcc32418a4a0b10fbe7bc8f97eafb6182060
matt venn <matt@mattvenn.net>
2017-08-25 18:37:10 +0200

    .gitignore and cleanup control

--- control.py
+++ control.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python
 import serial
 import csv
+import struct
+
 ser=serial.Serial()
 ser.port="/dev/ttyUSB1"
 ser.baudrate=115200
 ser.timeout=1
 ser.open()
 
-#ser.setDTR(True)
-import struct
+STEP = 0
+LED = 1
+COUNT = 2
+
+def step():
+    # take a step
+    ser.write(struct.pack('B', STEP ));
+    data = ser.read(1)
+
+def read(reg):
+    ser.write(struct.pack('B', reg ));
+    data = ser.read(1) # 1 byte hard coded
+    data, = struct.unpack('B', data)
+    return data
+
 with open("dumpvar" + '.csv', 'wb') as csvfile:
     wr = csv.writer(csvfile, delimiter=',')
     for i in range(2000):
-        # take a step
-        ser.write(struct.pack('B', 0 ));
-        data = ser.read(1)
-
-        #read led reg
-        ser.write(struct.pack('B', 1 ));
-        data = ser.read(1)
-        data, = struct.unpack('B', data)
-        leds = data
-
-        ser.write(struct.pack('B', 2 ));
-        data = ser.read(1)
-        data, = struct.unpack('B', data)
-        count = data
+        leds = read(LED)
+        count = read(COUNT)
+        step()
 
         wr.writerow([i, leds, count])
